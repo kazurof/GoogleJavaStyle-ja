1c212eb9c16cc258c902a8fe65d37a6dc8d3f544
kazuro fukuhara <kazuro.fukuhara@gmail.com>
2014-05-16 01:06:32 +0900

    headタグに追記。

--- makehtml.rb
+++ makehtml.rb
@@ -17,6 +17,9 @@ __END__
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Google Java Style (非公式和訳)</title>
+<meta name="keywords" content="java, Google,コーディング規約">
+<meta name="description" content="本文書はGoogleのJavaコーディング規約である Google Java Style の非公式和訳です。">
 </head>
 <body>
 <%= result %>
